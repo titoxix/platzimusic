5d355f2213e88c240a45715f95d1df4bb7ebc55c
Matias Martinez <mat.360z@gmail.com>
2018-11-19 09:55:26 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,84 @@
+<template lang="pug">
+  #app
+    img(src='https://github.com/titoxix/platzimusic/dist/logo.png')
+    h1 PlatziMusic
+    select(v-model="selectedCountry")
+      option(v-for="country in countries" v-bind:value="country.value") {{ country.name }} 
+    Spinner(v-show="loading")
+    ul
+      Artist(v-for='artist in artists' v-bind:artist='artist' v-bind:key='artist.mbid')
+</template>
+
+<script>
+import Artist from "./components/Artist";
+import Spinner from "./components/Spinner";
+import getArtists from "./api";
+
+export default {
+  name: "app",
+  data() {
+    return {
+      artists: [],
+      countries: [
+        { name: "Argentina", value: "argentina" },
+        { name: "Colombia", value: "colombia" },
+        { name: "España", value: "spain" }
+      ],
+      selectedCountry: "argentina",
+      loading: true
+    };
+  },
+  methods: {
+    refreshArtists() {
+      const self = this;
+      this.loading = true;
+      this.artists = [];
+      getArtists(this.selectedCountry).then(function(artists) {
+        self.artists = artists;
+        self.loading = false;
+      });
+    }
+  },
+  mounted() {
+    this.refreshArtists();
+  },
+  watch: {
+    selectedCountry() {
+      this.refreshArtists();
+    }
+  },
+  components: {
+    Artist,
+    Spinner
+  }
+};
+</script>
+
+<style lang="stylus">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/api/config.js
@@ -0,0 +1,6 @@
+export default {
+    appName: 'platzimusic',
+    apiKey: 'ad38ac1a03521dc5a019f44c13a121f7',
+    secret: 'c3cc679841fa27a7802c6497ac57762b',
+    RegisteredTo: 'tito2505'
+}
